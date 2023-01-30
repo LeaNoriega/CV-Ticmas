17cbfb8be976f08ba5a7a14235204274e0f17e21
LeaNoriega <123911687+LeaNoriega@users.noreply.github.com>
2023-01-29 22:34:31 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = document.getElementById("lenguajes");
+
+
+function desplegarL2() {
+    if  (document.getElementById("lenguajes").style.display != "block"){
+      document.getElementById("lenguajes").style.display="block"
+    } else {document.getElementById("lenguajes").style.display = "none";
+
+    }
+       
+  }
+
+function desplegarL1() {
+    if  (document.getElementById("habilidades").style.display != "block"){
+      document.getElementById("habilidades").style.display="block"
+    } else {document.getElementById("habilidades").style.display = "none";
+
+    }
+       
+  }
